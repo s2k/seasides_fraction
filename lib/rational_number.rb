978e01eff0d92f4bbfb3379ab9be9fd3c194ca23
Stephan Kämper <the.tester@seasidetesting.com>
2016-10-16 22:12:55 +0200

    can now add two rational numbers

--- lib/rational_number.rb
+++ lib/rational_number.rb
@@ -1,8 +1,22 @@
 class RationalNumber
   attr_reader :numerator, :denominator
 
-  def initialize(numerator = 0, denominator = 0)
-    @numerator   = numerator
-    @denominator = denominator
+  def self.zero
+    new(0,1)
+  end
+
+  def initialize(numerator = 0, denominator = 1)
+    gcd = numerator.gcd denominator
+    @numerator   = numerator / gcd
+    @denominator = denominator / gcd
+  end
+
+  def ==(other)
+    numerator == other.numerator && denominator == other.denominator
+  end
+
+  def +(other)
+    gcd = denominator.gcd other.denominator
+    self.class.new(numerator * (other.denominator / gcd) + other.numerator * (denominator / gcd), (denominator * other.denominator) / gcd)
   end
 end
--- spec/rational_number_spec.rb
+++ spec/rational_number_spec.rb
@@ -11,4 +11,22 @@ describe RationalNumber do
     expect(RationalNumber.new(3, 7).numerator).to be 3
     expect(RationalNumber.new(5, 7).denominator).to be 7
   end
+
+  it 'automatically reduces initial values' do
+    expect(RationalNumber.new(6,8)). to eq RationalNumber.new(3,4)
+  end
+
+  it 'has a method to create the value zero' do
+    expect(RationalNumber.zero).to eq RationalNumber.new(0,1)
+  end
+
+  it 'can add two zeroes' do
+    n1 = RationalNumber.zero
+    n2 = RationalNumber.zero
+    expect(n1 + n2).to eq RationalNumber.zero
+  end
+
+  it 'adds two rationals' do
+    expect(RationalNumber.new(3,7) + RationalNumber.new(5,11)).to eq RationalNumber.new(68,77)
+  end
 end
