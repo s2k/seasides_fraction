2937fbcb6f9c9454fd011ce655eadf8b24324a77
Stephan Kämper <the.tester@seasidetesting.com>
2016-10-16 20:29:35 +0200

    can create rational numbers

--- lib/rational_number.rb
+++ lib/rational_number.rb
@@ -1,5 +1,8 @@
 class RationalNumber
-  def initialize numerator = 0, denominator = 0
+  attr_reader :numerator, :denominator
 
+  def initialize(numerator = 0, denominator = 0)
+    @numerator   = numerator
+    @denominator = denominator
   end
 end
--- spec/rational_number_spec.rb
+++ spec/rational_number_spec.rb
@@ -6,6 +6,9 @@ describe RationalNumber do
   end
 
   it 'can create a number with preset numerator & denominator' do
-    n = RationalNumber.new 1, 2
+    expect(RationalNumber.new(1, 2).numerator).to be 1
+    expect(RationalNumber.new(2, 3).denominator).to be 3
+    expect(RationalNumber.new(3, 7).numerator).to be 3
+    expect(RationalNumber.new(5, 7).denominator).to be 7
   end
 end
