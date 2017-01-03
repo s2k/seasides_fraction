5497a5788cd1b635a59a81f2157a2f99ed9bd0fb
Stephan Kämper <the.tester@seasidetesting.com>
2017-01-03 16:12:26 +0100

    Add more specs & fix behaviour - yes, too much don in this commit

--- lib/rational_number.rb
+++ lib/rational_number.rb
@@ -16,9 +16,21 @@ class RationalNumber
 
   def initialize(numerator = 0, denominator = 1)
     return NaN if denominator == 0
-    gcd = numerator.gcd denominator
+    normalize(denominator, numerator)
+  end
+
+  def normalize(denominator, numerator)
+    gcd          = numerator.gcd denominator
     @numerator   = numerator / gcd
     @denominator = denominator / gcd
+    normalize_sign
+  end
+
+  def normalize_sign
+    if @denominator < 0
+      @denominator = -@denominator
+      @numerator   = -@numerator
+    end
   end
 
   def ==(other)
--- spec/rational_number_spec.rb
+++ spec/rational_number_spec.rb
@@ -3,15 +3,24 @@ require 'spec_helper'
 describe RationalNumber do
   it { is_expected.to be_a RationalNumber }
 
-  it 'can create a number with preset numerator & denominator' do
+  it 'can create real a rational number' do
     expect(RationalNumber.new(1, 2).numerator).to be 1
     expect(RationalNumber.new(2, 3).denominator).to be 3
     expect(RationalNumber.new(3, 7).numerator).to be 3
     expect(RationalNumber.new(5, 7).denominator).to be 7
   end
 
+  it 'can create integer values' do
+    expect(RationalNumber.new(7, 7).denominator).to be 1
+    expect(RationalNumber.new(-3, 3).numerator).to be -1
+    expect(RationalNumber.new(1, -1).numerator).to be -1
+    expect(RationalNumber.new(-1, -1).numerator).to be 1
+    expect(RationalNumber.new(-2, -1).numerator).to be 2
+  end
+
   it 'automatically reduces initial values' do
     expect(RationalNumber.new(6,8)).to eq RationalNumber.new(3,4)
+    expect(RationalNumber.new(6,-8)).to eq RationalNumber.new(-3,4)
   end
 
   it 'has a method to create the value zero' do
